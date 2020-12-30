c9c8451fdd62737be096d92f77ab40e43b505e3e
xee <xeeongithub@gmail.com>
2020-12-30 11:27:27 +0100

    20201230

--- lib/Util/UtilClass.pm
+++ lib/Util/UtilClass.pm
@@ -11,8 +11,8 @@ use Moo;
 #--------------------------------------------------------------------------------------------------------------------------
 # pkg system
 #--------------------------------------------------------------------------------------------------------------------------
-use Text::Xslate;
-use Types::Standard qw(Str InstanceOf HashRef);
+use Text::Xslate qw(mark_raw);
+use Types::Standard qw(Str InstanceOf HashRef Int);
 use Data::Dumper;
 use utf8;
 
@@ -23,19 +23,25 @@ use utf8;
 #--------------------------------------------------------------------------------------------------------------------------
 # attributes
 #--------------------------------------------------------------------------------------------------------------------------
-has pkg_name      => ( is => 'ro', lazy => 1, default => __PACKAGE__,             isa => Str     );
-has base_name     => ( is => 'ro', lazy => 1, default => __FILE__,                isa => Str     );
-has data          => ( is => 'rw', lazy => 1, default => sub{ {} },               isa => HashRef );
-has class         => ( is => 'rw', lazy => 1, default => '',                      isa => Str     );
-has attribut      => ( is => 'rw', lazy => 1, default => '',                      isa => Str     );
-has parent_dir    => ( is => 'rw', lazy => 1, default => '',                      isa => Str     );
-has template_file => ( is => 'rw', lazy => 1, default => 'libs/Utils/UtilClass.tx', isa => Str     );
+has pkg_name      => ( is => 'ro', lazy => 1, default => __PACKAGE__,                      isa => Str     );
+has base_name     => ( is => 'ro', lazy => 1, default => __FILE__,                         isa => Str     );
+has data          => ( is => 'rw', lazy => 1, default => sub{ {} },                        isa => HashRef );
+has class         => ( is => 'rw', lazy => 1, default => '',                               isa => Str     );
+has attribut      => ( is => 'rw', lazy => 1, default => '',                               isa => Str     );
+has parent_dir    => ( is => 'rw', lazy => 1, default => '',                               isa => Str     );
+has class_default => ( is => 'ro', lazy => 1, default => 0,                                isa => Int     );
+has template_file => ( is => 'rw', lazy => 1, default => 'lib/Util/UtilClass.tx',          isa => Str     );
+has template      => ( is => 'rw', lazy => 1, default => 'lib/Util/UtilClassDefault.tx',   isa => Str     );
+
+
+
 
 #--------------------------------------------------------------------------------------------------------------------------
 # f* default builder
 #---
 sub BUILD {
   my $self = shift;
+  binmode STDOUT, ':utf8';
 }
 
 #--------------------------------------------------------------------------------------------------------------------------
@@ -45,20 +51,17 @@ sub do_make {
   my $self = shift;
   print "[$0][do_make] - inici\n";
 
-  my $ok_class    = $self->_validate_class;
-  my $ok_attribut = $self->_validate_attributes;
+  my $ok_class      = $self->_validate_class;
+  my $ok_attribut   = $self->_validate_attributes;
+  my $ok_parent_dir = $self->_validate_parent_dir;
 
   # validate class
-  print "class..... ok - " . $ok_class      . " - value: "  . $self->class     . "\n";
-  print "attribut.. ok - " . $ok_attribut . " - values: " . $self->attribut . "\n";
+  print "class........ ok - " . $ok_class      . " - value: " . $self->class    . "\n";
+  print "attribut..... ok - " . $ok_attribut   . " - value: " . $self->attribut . "\n";
+  # print "parent_dir... ok - " . $ok_parent_dir . " - value: " . $self->_validate_parent_dir . "\n";
 
   # do make class or package
-  if ( $ok_class && $ok_attribut ) {
-
-  # show message error bad aruguments input
-  } else {
-
-  }
+  $self->do_make_class_and_attributes if $ok_class and $ok_attribut;
 
 
   print "[$0][do_make] - final\n";
@@ -71,7 +74,15 @@ sub do_make_class_and_attributes  {
   my $self = shift;
   print "[$0][do_make_class_and_attributes] - inici\n";
 
+  die "[$0][do_make_class_and_attributes] I can't open template: " . $self->template_file . "\n" unless -r $self->template_file;
 
+  my $template_out = $self->data->{_class_file_};
+  my $tx = Text::Xslate->new( type => 'text', input_layer => ':utf8' );
+  my $template_file = ( $self->class_default ) ? $self->template : $self->template_file ;
+
+
+  print $tx->render( $template_file, $self->data );  
+ 
   print "[$0][do_make_class_and_attributes] - final\n";
 }
 
@@ -86,7 +97,27 @@ sub _validate_class {
     $self->data->{_class_name_} = $self->class( ucfirst($self->class) );
     $return = 1;
   }
+  return $return;
+}
 
+#--------------------------------------------------------------------------------------------------------------------------
+# f* _validate_parent_dir:
+#---
+sub _validate_parent_dir {
+  my $self   = shift;
+  my $dir    = 'lib';
+  my $return = 1;
+
+  unless ( -d $dir . '/' . $self->parent_dir ) {
+    $return = mkdir $dir . '/' . $self->parent_dir;   
+    print "[0][_validate_parent_dir] Error when crate a parent_dir: " . $dir . '/' . $self->parent_dir . "\n$!\n" if !$return;
+    $self->data->{_class_file_} = $dir . '/' . $self->class . '.pm';
+    $self->data->{_class_pkg_}  = $dir . '::' . $self->class;
+  } else {
+    $self->data->{_class_file_} = $dir . '/'  . $self->parent_dir . '/'  . $self->class . '.pm';
+    $self->data->{_class_pkg_}  = $dir . '::' . $self->parent_dir . '::' . $self->class;
+  }
+  
   return $return;
 }
 
@@ -107,21 +138,22 @@ sub _validate_attributes {
 
     my @list_attribute = split /:/, $attributes[$i];
 
-    if ( $list_attribute[0] ne '' ) {
+    if ( $list_attribute[$i] ne '' ) {
+      print "--> " . $list_attribute[2] . "\n";
       # set value to hash
-      $self->data->{_list_attributes_}->{$attributes[$i]} = {
-        'name' => $list_attribute[0],
-        'type' => $list_attribute[1] || '',
-        'init' => $list_attribute[2] || '',
-      };
-
+      $self->data->{_list_attributes_}->{$list_attribute[$i]} =  
+        { 'name' => $list_attribute[0],
+          'type' => $list_attribute[1] || 'Any',
+          'init' => $list_attribute[2] || 'undef',
+        }
+      ;
     } else {
       $end = 0;
     }
     $i++;
   }
 
-  print "[$0][_validate_attributes] Error invalid attribute_type or attribute_init\n" if !$end;
+  print "[$0][_validate_attributes] Error invalid attribute: name, type or init\n" if !$end;
   return $end;
 }
 
--- main_utils_class_util.pl
+++ main_utils_class_util.pl
@@ -26,13 +26,14 @@ use Getopt::Long qw(GetOptions);
 #----------------------------------------------------------------------------------------------------------
 
 # var
-my ( $class, $attribut, $parent_dir ) = ( '', '', '' );
+my ( $class, $attribut, $parent_dir, $class_default ) = ( '', '', '', 0 );
 
 # read argument
 GetOptions(
-  'class=s'      => \$class,
-  'attributes=s' => \$attribut,
-  'parent_dir=s' => \$parent_dir,
+  'class=s'         => \$class,
+  'attributes=s'    => \$attribut,
+  'parent_dir=i'    => \$parent_dir,
+  'class_default=i' => \$class_default,
 );
 
 # error class name is empty
@@ -41,9 +42,10 @@ GetOptions(
 
 # call main test
 my $pkg_class_util = lib::Util::UtilClass->new (
-  class      => $class,
-  attribut   => $attribut,
-  parent_dir => $parent_dir
+  class         => $class,
+  attribut      => $attribut,
+  parent_dir    => $parent_dir,
+  class_default => $class_default
 )->do_make;
 
 
@@ -56,8 +58,9 @@ my $pkg_class_util = lib::Util::UtilClass->new (
 #---
 sub msg_error {
   my $msg = $_[0] || '';
-  say "perl $0 --class <class_name> --attributes [<attrib_01:type:init_value>] --method [<method_01:arg_0:arg_01:arg_n>]
-  \nor\n./$0 --class <class_name> --attributes [<attrib_01:type:init_value>] --method [<method_01:arg_0:arg_01:arg_n>]";
+  say "perl $0 --class <class_name> --attributes [<attrib_01:type:init_value>] --method [<method_01:arg_0:arg_01:arg_n>] --class_default <1|0>
+  \nor\n./$0 --class <class_name> --attributes [<attrib_01:type:init_value>] --method [<method_01:arg_0:arg_01:arg_n>] --class_default <1|0>";
+
   say " *INFO* $msg" if $msg ne '';
   exit;
 }
